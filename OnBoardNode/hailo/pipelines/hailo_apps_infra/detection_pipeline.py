5849e4a04f7740e20d4c988f7310b9e8d259e6d1
Frederick Andrews <freddywandrews@gmail.com>
2025-03-02 14:25:12 -0500

    tiling starter code

--- OnBoardNode/hailo/pipelines/hailo_apps_infra/detection_pipeline.py
+++ OnBoardNode/hailo/pipelines/hailo_apps_infra/detection_pipeline.py
@@ -1,5 +1,6 @@
 import gi
-gi.require_version('Gst', '1.0')
+
+gi.require_version("Gst", "1.0")
 from gi.repository import Gst, GLib
 import os
 import argparse
@@ -13,11 +14,12 @@ from hailo_apps_infra.hailo_rpi_common import (
     get_default_parser,
     detect_hailo_arch,
 )
-from hailo_apps_infra.gstreamer_helper_pipelines import(
+from hailo_apps_infra.gstreamer_helper_pipelines import (
     QUEUE,
     SOURCE_PIPELINE,
     INFERENCE_PIPELINE,
     INFERENCE_PIPELINE_WRAPPER,
+    TILE_CROPPER_PIPELINE,
     TRACKER_PIPELINE,
     USER_CALLBACK_PIPELINE,
     DISPLAY_PIPELINE,
@@ -25,14 +27,14 @@ from hailo_apps_infra.gstreamer_helper_pipelines import(
 from hailo_apps_infra.gstreamer_app import (
     GStreamerApp,
     app_callback_class,
-    dummy_callback
+    dummy_callback,
 )
-
 
 
 # -----------------------------------------------------------------------------------------------
 # User Gstreamer Application
 # -----------------------------------------------------------------------------------------------
+
 
 # This class inherits from the hailo_rpi_common.GStreamerApp class
 class GStreamerDetectionApp(GStreamerApp):
@@ -52,28 +54,32 @@ class GStreamerDetectionApp(GStreamerApp):
         nms_score_threshold = 0.3
         nms_iou_threshold = 0.45
 
-
         # Determine the architecture if not specified
         if args.arch is None:
             detected_arch = detect_hailo_arch()
             if detected_arch is None:
-                raise ValueError("Could not auto-detect Hailo architecture. Please specify --arch manually.")
+                raise ValueError(
+                    "Could not auto-detect Hailo architecture. Please specify --arch manually."
+                )
             self.arch = detected_arch
             print(f"Auto-detected Hailo architecture: {self.arch}")
         else:
             self.arch = args.arch
 
-
         if args.hef_path is not None:
             self.hef_path = args.hef_path
         # Set the HEF file path based on the arch
         elif self.arch == "hailo8":
-            self.hef_path = os.path.join(self.current_path, '../resources/yolov8m.hef')
+            self.hef_path = os.path.join(self.current_path, "../resources/yolov8m.hef")
         else:  # hailo8l
-            self.hef_path = os.path.join(self.current_path, '../resources/yolov8s_h8l.hef')
+            self.hef_path = os.path.join(
+                self.current_path, "../resources/yolov8s_h8l.hef"
+            )
 
         # Set the post-processing shared object file
-        self.post_process_so = os.path.join(self.current_path, '../resources/libyolo_hailortpp_postprocess.so')
+        self.post_process_so = os.path.join(
+            self.current_path, "../resources/libyolo_hailortpp_postprocess.so"
+        )
         self.post_function_name = "filter_letterbox"
         # User-defined label JSON file
         self.labels_json = args.labels_json
@@ -92,28 +98,78 @@ class GStreamerDetectionApp(GStreamerApp):
         self.create_pipeline()
 
     def get_pipeline_string(self):
-        source_pipeline = SOURCE_PIPELINE(self.video_source, self.video_width, self.video_height)
+        source_pipeline = SOURCE_PIPELINE(
+            self.video_source, self.video_width, self.video_height
+        )
         detection_pipeline = INFERENCE_PIPELINE(
             hef_path=self.hef_path,
             post_process_so=self.post_process_so,
             post_function_name=self.post_function_name,
             batch_size=self.batch_size,
             config_json=self.labels_json,
-            additional_params=self.thresholds_str)
+            additional_params=self.thresholds_str,
+        )
         detection_pipeline_wrapper = INFERENCE_PIPELINE_WRAPPER(detection_pipeline)
         tracker_pipeline = TRACKER_PIPELINE(class_id=1)
         user_callback_pipeline = USER_CALLBACK_PIPELINE()
-        display_pipeline = DISPLAY_PIPELINE(video_sink=self.video_sink, sync=self.sync, show_fps=self.show_fps)
+        display_pipeline = DISPLAY_PIPELINE(
+            video_sink=self.video_sink, sync=self.sync, show_fps=self.show_fps
+        )
 
         pipeline_string = (
-            f'{source_pipeline} ! '
-            f'{detection_pipeline_wrapper} ! '
-            f'{tracker_pipeline} ! '
-            f'{user_callback_pipeline} ! '
-            f'{display_pipeline}'
+            f"{source_pipeline} ! "
+            f"{detection_pipeline_wrapper} ! "
+            f"{tracker_pipeline} ! "
+            f"{user_callback_pipeline} ! "
+            f"{display_pipeline}"
         )
         print(pipeline_string)
         return pipeline_string
+
+    def get_tiled_pipeline_string(self):
+        source_pipeline = SOURCE_PIPELINE(
+            self.video_source,
+            self.video_width,
+            self.video_height,
+            preserve_input_resolution=True,
+        )
+        tile_cropper_pipeline = TILE_CROPPER_PIPELINE()
+        detection_pipeline = INFERENCE_PIPELINE(
+            hef_path=self.hef_path,
+            post_process_so=self.post_process_so,
+            post_function_name=self.post_function_name,
+            batch_size=self.batch_size,
+            config_json=self.labels_json,
+            additional_params=self.thresholds_str,
+        )
+
+        tracker_pipeline = TRACKER_PIPELINE(class_id=1)
+        user_callback_pipeline = USER_CALLBACK_PIPELINE()
+        display_pipeline = DISPLAY_PIPELINE(
+            video_sink=self.video_sink, sync=self.sync, show_fps=self.show_fps
+        )
+
+        PIPELINE = "gst-launch-1.0 $source_element ! \
+    video/x-raw,pixel-aspect-ratio=1/1,format=RGB ! queue leaky=no max-size-buffers=3 max-size-bytes=0 max-size-time=0 ! \
+    $TILE_CROPPER_ELEMENT hailotileaggregator flatten-detections=true iou-threshold=$iou_threshold name=agg \
+    cropper. ! queue leaky=no max-size-buffers=3 max-size-bytes=0 max-size-time=0 ! agg. \
+    cropper. ! $DETECTION_PIPELINE ! agg. \
+    agg. ! queue leaky=no max-size-buffers=3 max-size-bytes=0 max-size-time=0 ! \
+    hailooverlay qos=false ! \
+    queue leaky=no max-size-buffers=3 max-size-bytes=0 max-size-time=0 ! videoconvert qos=false ! \
+    fpsdisplaysink video-sink=$video_sink_element name=hailo_display sync=$sync_pipeline text-overlay=false $additional_parameters"
+
+        pipeline_string = (
+            f"{source_pipeline} ! "
+            f"{tile_cropper_pipeline} ! "
+            f"{detection_pipeline} ! "
+            f"{tracker_pipeline} ! "
+            f"{user_callback_pipeline} ! "
+            f"{display_pipeline}"
+        )
+        print(pipeline_string)
+        return pipeline_string
+
 
 if __name__ == "__main__":
     # Create an instance of the user app callback class

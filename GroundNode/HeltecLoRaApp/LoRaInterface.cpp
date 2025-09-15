b99e64b3553d35c7eee65d0d804547a1791f5d35
Josue Dazogbo <jdazo054@uottawa.ca>
2025-09-15 16:34:34 -0400

    Added LoraInterface implementation

---
+++ GroundNode/HeltecLoRaApp/LoRaInterface.cpp
@@ -0,0 +1,62 @@
+/* 
+ * File:   LoRaInterface.c
+ * Author: JDazogbo
+ *
+ * Created on September 11, 2025, 1:31 PM
+ */
+
+
+/*---------------- Include Files ------------------*/
+
+#include <LoRaInterface.h>
+
+char LoRaInterface::txpacket[BUFFER_SIZE];
+char LoRaInterface::rxpacket[BUFFER_SIZE];
+RadioEvents_t LoRaInterface::RadioEvents;
+
+int16_t LoRaInterface::txNumber;
+int16_t LoRaInterface::rssi;
+int16_t LoRaInterface::rxSize;
+bool LoRaInterface::lora_idle = true;
+
+
+// Free function callback for RxDone
+void onRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr) {
+    memcpy(LoRaInterface::rxpacket, payload, size);
+    LoRaInterface::rxpacket[size] = '\0';
+    LoRaInterface::rssi = rssi;
+    LoRaInterface::rxSize = size;
+    Radio.Sleep();
+    Serial.printf("\r\nreceived packet \"%s\" with rssi %d , length %d\r\n", LoRaInterface::rxpacket, LoRaInterface::rssi, LoRaInterface::rxSize);
+    LoRaInterface::lora_idle = true;
+}
+
+/*---------------- LoRa Module Class Declaration ------------------*/
+
+LoRaInterface::LoRaInterface(uint8_t rfFrequency, uint8_t outputPower) {
+    Mcu.begin(HELTEC_BOARD, SLOW_CLK_TPYE);
+    txNumber = 0;
+    rssi = 0;
+    lora_idle = true;
+    RadioEvents.RxDone = onRxDone;
+    Radio.Init(&RadioEvents);
+    Radio.SetChannel(rfFrequency);
+    Radio.SetPublicNetwork(true);
+    Radio.SetRxConfig(MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                     LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                     LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                     BUFFER_SIZE, true, 0, 0, LORA_IQ_INVERSION_ON, true);
+}
+
+void LoRaInterface::getMessages() {
+    if (lora_idle) {
+        lora_idle = false;
+        Serial.println("into RX mode");
+        Radio.Rx(0);
+    }
+    Radio.IrqProcess();
+}
+
+
+
+
